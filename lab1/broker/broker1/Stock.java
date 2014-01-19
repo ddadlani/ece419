215ce6557176339dcf1e8bc05892de328f548ba3
divd <divya.dadlani@gmail.com>
2014-01-18 19:07:25 -0500

    Part 1 works

--- lab1/broker/broker1/Stock.java
+++ lab1/broker/broker1/Stock.java
@@ -3,37 +3,36 @@ import java.io.*;
 import java.lang.*;
 
 public class Stock {
-	private String symbol;
-	private Long quote;
-	
-	public Stock (String symbol, Long quote) {
-		this.symbol = symbol;
-		this.quote = quote;
-	}
+        private String symbol;
+        private Long quote;
+        
+        public Stock ()
+        {
+        }
+        public Stock (String symbol, Long quote) {
+                this.symbol = symbol;
+                this.quote = quote;
+        }
 
-	public Long getQuote() {
-		return this.quote;
-	}
-	
-	public String getSymbol() {
-		return this.symbol;
-	}
-	
-	public void setSymbol(String symbol_) {
-		this.symbol = symbol_;
-	}
-	
-	public void setQuote(Long quote_) {
-		this.symbol = quote_;
-	}
-	
-	@Override
-	public boolean equals(Stock other) {
-		if (other.symbol == this.symbol)
-			return true;
-		else
-			return false;
-	}	
-	
+        public Long getQuote() {
+                return this.quote;
+        }
+        
+        public String getSymbol() {
+                return this.symbol;
+        }
+        
+        public void setSymbol(String symbol_) {
+                this.symbol = symbol_;
+        }
+        
+        public void setQuote(Long quote_) {
+                this.quote = quote_;
+        }
+        
+        //@Override
+        //public boolean equals(Stock other) {
+        //        return (other.symbol.equals(this.symbol));
+        //        }        
 }
-	
+        
